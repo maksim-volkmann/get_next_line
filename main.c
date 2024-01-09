47456ef584ba1af1c7b6989bd2c7c972b3944afb
maksim-volkmann <auksinistonic@gmail.com>
2024-01-09 21:56:44 +0100

    main test

--- main.c
+++ main.c
@@ -1,13 +1,40 @@
-#include <fcntl.h>
+// #include <fcntl.h>
+// #include <unistd.h>
+// #include "get_next_line.h"
+
+// int	main(void)
+// {
+// 	int	fd;
+
+// 	fd = open("test.txt", 0_RDONLY);
+// 	if (fd == -1)
+// 		return (1);
+
+// }
+
 #include <unistd.h>
-#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdlib.h>
 
-int	main(void)
-{
+#define BUFFER_SIZE 5
+
+int main(void) {
 	int fd;
+	ssize_t read_bytes;
+	char buffer[BUFFER_SIZE];
 
-	fd = open("test.txt", 0_RDONLY);
-	if (fd == -1)
-		return (1);
+	// Open the file
+	fd = open("test.txt", O_RDONLY);
+	// if (fd == -1)
+	// 	return (1);
 
+	// Read from the file and write to the terminal
+	while ((read_bytes = read(fd, buffer, BUFFER_SIZE)) > 0) {
+		write(STDOUT_FILENO, buffer, read_bytes);
+	}
+
+	// Close the file
+	close(fd);
+
+	return 0;
 }
