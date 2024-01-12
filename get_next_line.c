5673229404a2405eaaf6da705b8c3ce7e1312da6
Maksim Volkmann <mvolkman@2-H-9.42heilbronn.de>
2024-01-12 17:18:47 +0100

    add storage check

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mvolkman <mvolkman@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 18:07:44 by mvolkman          #+#    #+#             */
-/*   Updated: 2024/01/11 12:45:41 by mvolkman         ###   ########.fr       */
+/*   Updated: 2024/01/12 16:52:20 by mvolkman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 char	*get_next_line(int fd)
 {
-	static char	buffer;
-	char		line;
+	static char	*storage = NULL;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	if (!storage)
+	{
+		storage = malloc(1);
+		if (!storage)
+			return (NULL);
+		storage[0] = '\0';
+	}
 }
